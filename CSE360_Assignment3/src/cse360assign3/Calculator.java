1693d73d3ac1a87c9ef2c468f2d07a295da1b370
ThienDuc <ThienDuc@www.007guard.com>
2016-02-22 15:16:45 -0700

    Second update of third version of Calculator

--- CSE360_Assignment3/src/cse360assign3/Calculator.java
+++ CSE360_Assignment3/src/cse360assign3/Calculator.java
@@ -3,60 +3,91 @@ package cse360assign3;
 /** Creating a calculator and commit to GitHub
  * @author Thien Duc Phung 
  * 		PIN#: 703
- * @version 2/20/2016
+ * @version 2/22/2016
  * 
  */
 
 public class Calculator 
 {
 	private int total;
+	private String math;
 	
-	// Calculator method
+	/**
+	 * Calculator method
+	 */
 	public Calculator() 
 	{
 		total = 0;  // not needed - included for clarity
 	}
 	
-	// Return total value 
+	/**
+	 * Return total value 
+	 * @return current value of total
+	 */
 	public int getTotal() 
 	{
 		return total;
 	}
 	
-	// Adding function of the calculator
+	/**
+	 * Adding function of the calculator
+	 * @param value - parameter from user
+	 */
 	public void add(int value) 
 	{
 		total = total + value;
+		
+		math = " + " + value;
 	}
 	
-	// Subtracting function of the calculator
+	/**
+	 * Subtracting function of the calculator
+	 * @param value - parameter from user
+	 */
 	public void subtract(int value) 
 	{
 		total = total - value;
+		
+		math = " - " + value;
 	}
 	
-	// Multiplying function of the calculator
+	/**
+	 * Multiplying function of the calculator
+	 * @param value - parameter from user
+	 */
 	public void multiply(int value) 
 	{
 		total = total * value;
+		
+		math = " * " + value;
 	}
 	
-	// Diving function of the calculator
+	/**
+	 * Diving function of the calculator
+	 * @param value parameter from user
+	 */
 	public void divide(int value) 
 	{
 		if (value == 0)
 		{
 			total = 0;
+			
+			math = " / " + value;
 		}
 		else 
 		{
 			total = total / value;
+			
+			math = " / " + value;
 		}
 	}
 	
-	// Contain all the history of using any mathematical methods 
+	/**
+	 * Contain all the history of using any mathematical methods 
+	 * @return math string contain the appropriate sign and value
+	 */
 	public String getHistory() 
 	{
-		return "";
+		return math;
 	}
 }
--- CSE360_Assignment3/src/cse360assign3/CalculatorTest.java
+++ CSE360_Assignment3/src/cse360assign3/CalculatorTest.java
@@ -3,7 +3,7 @@ package cse360assign3;
 /** Creating a calculator test case and commit to GitHub
  * @author Thien Duc Phung 
  * 		PIN#: 703
- * @version 2/20/2016
+ * @version 2/22/2016
  * 
  */
 
@@ -13,7 +13,10 @@ import org.junit.Test;
 
 public class CalculatorTest 
 {
-	@Test // Junit test case to test Calculator method to see if it return NULL or not
+	/**
+	 * Junit test case to test Calculator method to see if it return NULL or not
+	 */
+	@Test
 	public void testCalculator()
 	{
 		Calculator TI83 = new Calculator();
@@ -21,7 +24,10 @@ public class CalculatorTest
 		assertNotNull("The value is not null", TI83);
 	}
 	
-	@Test // Junit test case to test getTotal() method
+	/**
+	 * Junit test case to test getTotal() method
+	 */
+	@Test 
 	public void testGetTotal()
 	{
 		Calculator TI83 = new Calculator();
@@ -29,7 +35,10 @@ public class CalculatorTest
 		assertNotEquals(1, TI83.getTotal());
 	}
 	
-	@Test // Junit test case to test add() method
+	/**
+	 * Junit test case to test add() method
+	 */
+	@Test 
 	public void testAdd()
 	{
 		Calculator TI83 = new Calculator();
@@ -40,7 +49,10 @@ public class CalculatorTest
 		assertEquals(6, TI83.getTotal());
 	}
 
-	@Test // Junit test case to test subtract() method
+	/**
+	 * Junit test case to test subtract() method
+	 */
+	@Test 
 	public void testSubtract()
 	{
 		Calculator TI83 = new Calculator();
@@ -50,7 +62,10 @@ public class CalculatorTest
 		assertEquals(-5, TI83.getTotal());
 	}
 	
-	@Test // Junit test case to test multiply() method
+	/**
+	 * Junit test case to test multiply() method
+	 */
+	@Test 
 	public void testMultiply()
 	{
 		Calculator TI83 = new Calculator();
@@ -61,7 +76,10 @@ public class CalculatorTest
 		assertEquals(4, TI83.getTotal());
 	} 
 	
-	@Test // Junit test case to test divide() method
+	/**
+	 * Junit test case to test divide() method
+	 */
+	@Test
 	public void testDivide()
 	{
 		Calculator TI83 = new Calculator();
@@ -72,9 +90,36 @@ public class CalculatorTest
 		assertNotEquals(1, TI83.getTotal());
 	}
 	
-	@Test // Junit test case to test getHistory() method
+	/*
+	 * Junit test case to test getHistory() method
+	 */
+	@Test 
 	public void testGetHistory()
 	{
-		fail("Not yet implemented");
+		int value = 0;
+		
+		Calculator TI83 = new Calculator();
+		
+		System.out.print(value);
+		
+		value = 4;
+		TI83.add(value);
+		System.out.print(TI83.getHistory());
+		
+		value = 2;
+		TI83.subtract(value);
+		System.out.print(TI83.getHistory());
+		
+		value = 0;
+		TI83.divide(value);
+		System.out.print(TI83.getHistory());
+		
+		value = 4;
+		TI83.subtract(value);
+		System.out.print(TI83.getHistory());
+		
+		value = 3;
+		TI83.multiply(value);
+		System.out.print(TI83.getHistory());
 	}
 }
